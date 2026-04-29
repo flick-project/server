d22f9ac03c88cca60ddffd31388166ce26270ddc
Hans Nilsson <hn222yi@student.lnu.se>
2026-04-29 16:48:07 +0200

    feat: add optional auth middleware

---
+++ src/middleware/optionalAuth.js
@@ -0,0 +1,28 @@
+/**
+ * @file Optional authentication middleware.
+ * @module middleware/optionalAuth
+ * @author Hans Nilsson
+ * @version 0.1.0
+ */
+
+import jwt from 'jsonwebtoken'
+
+/**
+ * Optionally attaches user data from JWT if a valid token is present.
+ * Continues without user data if no token or invalid token is provided.
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {(error: Error) => void} next - Express next middleware function.
+ */
+export const optionalAuth = (req, res, next) => {
+  try {
+    const [scheme, token] = req.headers.authorization?.split(' ') ?? []
+    if (scheme !== 'Bearer') {
+      throw new Error('Invalid authentication scheme.')
+    }
+    req.user = jwt.verify(token, process.env.JWT_SECRET)
+  } catch {
+    // Invalid token, continue without user.
+  }
+  next()
+}
